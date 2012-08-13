01eb86fef78b962199be5362aa508e33762b0a9c
Sean Redmond <github-smr@sneakemail.com>
2012-08-13 17:14:27 -0400

    Require a couple of new classes

--- lib/gugg-web_api-collection-db.rb
+++ lib/gugg-web_api-collection-db.rb
@@ -1,4 +1,6 @@
 require "gugg-web_api-collection-db/version"
+require "gugg-web_api-collection-db/language_code"
+require "gugg-web_api-collection-db/media_type"
 require "gugg-web_api-collection-db/object_context"
 require "gugg-web_api-collection-db/text_entry"
 require "gugg-web_api-collection-db/title_type"
