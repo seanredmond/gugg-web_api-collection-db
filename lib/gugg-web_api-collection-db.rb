f395ecca82c03bf6d7fa715ced09fd7f9be5d4e2
Sean Redmond <github-smr@sneakemail.com>
2012-08-14 15:05:09 -0400

    Add CollectionObject class and first tests

--- lib/gugg-web_api-collection-db.rb
+++ lib/gugg-web_api-collection-db.rb
@@ -2,10 +2,12 @@ require "gugg-web_api-collection-db/version"
 require "gugg-web_api-collection-db/language_code"
 require "gugg-web_api-collection-db/language"
 require "gugg-web_api-collection-db/media_type"
+require "gugg-web_api-collection-db/media_format"
 require "gugg-web_api-collection-db/object_context"
 require "gugg-web_api-collection-db/role"
 require "gugg-web_api-collection-db/text_entry"
 require "gugg-web_api-collection-db/title_type"
+require "gugg-web_api-collection-db/collection_object"
 
 module Gugg
   module WebApi
