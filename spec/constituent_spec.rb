b6ae81a43d915efd8e497b59d1da318a9122236b
Sean Redmond <github-smr@sneakemail.com>
2012-08-16 14:06:24 -0400

    Add some tests

--- spec/constituent_spec.rb
+++ spec/constituent_spec.rb
@@ -18,8 +18,33 @@ require 'gugg-web_api-collection-db'
 MDL = Gugg::WebApi::Collection::Db
 
 describe MDL::Constituent do
+  before :all do
+    @kandinsky = Constituent[1515]
+  end
+
   it "should return rows" do
     # There are 4054 constituents as of Aug 2012
     MDL::Constituent.all().count.should be >= 4054
   end
+
+  it "should have a last name" do
+    @kandinsky.last_name.should eq "Kandinksy"
+  end
+
+  it "should have a first name" do
+    @kandinsky.first_name.should eq "Vasily"
+  end
+
+  it "should have a middle name" do
+    @kandinsky.middle_name.should be_nil
+  end
+
+  it "should have a display name" do
+    @kandinsky.middle_name.should eq "Vasily Kandinsky"
+  end
+
+  it "should have a sort name" do
+    @kandinsky.alphasort.should eq "Kandinsky, Vasily"
+  end
+
 end
