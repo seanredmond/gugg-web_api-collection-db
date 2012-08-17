21735711a62d3149c6b38698d870dd5c99d884ea
Sean Redmond <github-smr@sneakemail.com>
2012-08-17 16:28:39 -0400

    Add Acquisition class and first tests

---
+++ lib/gugg-web_api-collection-db/acquisition.rb
@@ -0,0 +1,39 @@
+# Interface to Language Code table.
+#
+# Author:: Sean Redmond <sredmond@guggenheim.org>
+# Copyright:: Copyright © 2012 Solomon R. Guggenheim Foundation
+# License GPLv3
+#
+# The Language Code table maps TMS languages to ISO 639-1 equivalents
+
+module Gugg
+  module WebApi
+    module Collection
+      module Db
+        class Acquisition < Sequel::Model(:collection_acquisitions)
+          set_primary_key :acquisitionid
+          many_to_many :objects, :class=>CollectionObject, 
+            :join_table=>:collection_acquisitionxrefs, 
+            :left_key=>:acquisitionid, :right_key=>:objectid
+          include Linkable
+
+          def self.list
+            {
+              :acquisitions => all.
+                reject{|a| a.objects_dataset.count == 0}.
+                map{|a| a.as_resource({:per_page => 5})}
+            }
+          end
+
+          def as_resource(options = {})            
+            {
+              :id => pk,
+              :name => acquisition,
+              :_links => self_link
+            }
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/acquisition_spec.rb
@@ -0,0 +1,45 @@
+# Tests for tables without any extra implementation beyond Sequel
+#
+# Author:: Sean Redmond <sredmond@guggenheim.org>
+# Copyright:: Copyright © 2012 Solomon R. Guggenheim Foundation
+# License GPLv3
+
+require "rubygems"
+require "sequel"
+require "yaml"
+
+cfg = YAML.load_file('collection_db_spec.yml')
+db = cfg['db']['mysql']
+@DB = Sequel.mysql(db['db'], :user=>db['user'], :password=>db['password'], 
+  :host=>db['host'], :charset=>'utf8')
+
+require 'gugg-web_api-collection-db'
+
+MDL = Gugg::WebApi::Collection::Db
+
+describe MDL::Acquisition do
+  it "should return rows" do
+    # There are 12 acquisitions as of Aug 2012
+    MDL::Acquisition.all().count.should be >= 12
+  end
+
+  describe ".List" do
+    before :all do
+      @acq = MDL::Acquisition.list
+    end
+
+    it "should return a Hash" do
+      @acq.should be_an_instance_of Hash
+    end
+
+    it "has a list of Acquisition resources" do
+
+      @acq[:acquisitions].each do |a|
+        a.should be_an_instance_of Hash
+        a[:id].should be
+        a[:name].should be
+      end
+    end
+
+  end
+end
