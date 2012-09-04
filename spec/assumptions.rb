cb91963237247ab1bf826d34570893f4e868c85f
Sean Redmond <github-smr@sneakemail.com>
2012-09-04 15:52:58 -0400

    Add tests for assumptions
    Sometimes design decisions are based on assumptions that may be true of
    the currently available data but aren't certainly true for all data
    present and future. For instance, there are no objects that belong to
    more than one acquisition and there doesn't seem to be a reason to
    assume that any object ever would belong to more than one acquisition
    except that acquisitions can be funny things.
    
    This file records and tests for those assumptions. If any of these tests
    fail it would mean updating the API to reflect the unexpected change.

---
+++ spec/assumptions.rb
@@ -0,0 +1,52 @@
+# Tests for some assumptions about the data
+#
+# Author:: Sean Redmond <sredmond@guggenheim.org>
+# Copyright:: Copyright © 2012 Solomon R. Guggenheim Foundation
+# License GPLv3
+
+require "rubygems"
+require "sequel"
+require "yaml"
+
+cfg = YAML.load_file('collection_db_spec.yml')
+db = cfg['db']['mysql']
+@DB = Sequel.mysql(db['db'], :user=>db['user'], :password=>db['password'], 
+  :host=>db['host'], :charset=>'utf8')
+
+require 'gugg-web_api-collection-db'
+
+MDL = Gugg::WebApi::Collection::Db
+
+describe "Acquisitions" do
+  before :all do
+    @o = MDL::CollectionObject.all
+  end
+
+  # Objects should belong to 0 or 1 acquisitions. This is required for 
+  # CollectionObject#acquisition to have the right meaning since it returns
+  # one acquisition or nil. 
+  #
+  # The object's acquisitions association returns an array, but only because a 
+  # many-to-many relationship is required by the schema (two tables joined by 
+  # an intermediate mapping table). The assumption is that if the database were 
+  # designed differently each object could have one acquisition id. Following 
+  # this assumption the one object that CollectionObject#acquisitions returns is
+  # the first, and presumed only, member of the array. If any object were found 
+  # to belong to two acquisitions we would have to get rid of this method.
+  context "Acquisition counts" do
+    it "some objects should be in no acquisition" do
+      @o.select{|o| o.acquisitions.count == 0}.
+        should have_at_least(1).items
+    end
+
+    it "some objects should be in one acquisition" do
+      @o.select{|o| o.acquisitions.count == 1}.
+        should have_at_least(1).items
+    end
+
+    it "no objects should be in more than one acquisition" do
+      @o.select{|o| o.acquisitions.count > 1}.count.
+        should eq 0
+    end
+  end
+end
