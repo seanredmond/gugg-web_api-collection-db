905f0e5ac097f0da87259eb05fd5014e1f2f5088
Sean Redmond <github-smr@sneakemail.com>
2012-09-19 14:33:02 -0400

    Add as_resource method

--- lib/gugg-web_api-collection-db/exhibition.rb
+++ lib/gugg-web_api-collection-db/exhibition.rb
@@ -10,15 +10,37 @@ module Gugg
       module Db
         class Exhibition < Sequel::Model(:collection_tms_exhibitions)
           set_primary_key :exhibitionid
+          many_to_many :objects, :class=>CollectionObject, 
+            :join_table=>:collection_tms_exhobjxrefs, 
+            :left_key=>:exhibitionid, :right_key=>:objectid
+
           include Linkable
           include Collectible
           include Dateable
 
+          def after_initialize
+            @obj_dataset = objects_dataset
+            @obj_pages = nil
+          end
+
+          def name
+            exhtitle
+          end
+
+          def ours
+            objects_dataset.where(~ :departmentid => 7)
+          end
+
+          def not_ours
+            objects_dataset.where(:departmentid => 7)
+          end
+
           def as_resource(options = {})
+            objects_r = paginated_resource(options)
             {
-              :id => pk,
-              :name => exhtitle,
+              :name => name,
               :dates => date_resource(beginisodate, endisodate, displaydate),
+              :objects => objects_r,
               :_links => self_link(options)
             }
           end
