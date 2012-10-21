a327e864b9e2b178e3088c4beefac6c3fe2f2625
Sean Redmond <github-smr@sneakemail.com>
2012-10-21 18:40:20 -0400

    Start writing tests for the Location class

---
+++ spec/location_spec.rb
@@ -0,0 +1,25 @@
+# Tests for Location class
+#
+# Author:: Sean Redmond <sredmond@guggenheim.org>
+# Copyright:: Copyright © 2012 Solomon R. Guggenheim Foundation
+# License GPLv3
+
+require "rubygems"
+require "sequel"
+require "yaml"
+
+cfg = YAML.load_file('collection_db_spec.yml')
+db = cfg['db']['mysql']
+@DB = Sequel.mysql(db['db'], :user=>db['user'], :password=>db['password'], 
+  :host=>db['host'], :charset=>'utf8')
+
+require 'gugg-web_api-collection-db'
+
+MDL = Gugg::WebApi::Collection::Db
+
+describe MDL::Location do
+  it "return rows" do
+    # There are 54 locations as of Oct 2012
+    MDL::Location.all().count.should be >= 54
+  end
+end
