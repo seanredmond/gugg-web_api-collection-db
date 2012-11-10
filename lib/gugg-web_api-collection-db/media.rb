11d7356d20df38217708f0edba91b86e0a03b392
Sean Redmond <github-smr@sneakemail.com>
2012-11-10 12:12:28 -0500

    Methods to determine image orientation
    Add three methods -- orientation, is_landscape?, is_portrait? -- to indicate the orientation of the image.

--- lib/gugg-web_api-collection-db/media.rb
+++ lib/gugg-web_api-collection-db/media.rb
@@ -20,10 +20,59 @@ module Gugg
         # @copyright Copyright © 2012 Solomon R. Guggenheim Foundation
         # @license GPLv3
         class Media < Sequel::Model(:collection_object_images)
+          set_primary_key :mediaxrefid
           many_to_one :format, 
             :class => Gugg::WebApi::Collection::Db::MediaFormat, 
-              :key => :mediatypeid  
-        end
+              :key => :mediatypeid
+
+            ORIENTATION_LANDSCAPE = 0
+            ORIENTATION_PORTRAIT  = 1
+
+          # Returns a representation of the media object in a hash suitable for
+          # output as a JSON resource
+          #
+          # @param [Hash] options A hash of options to be passed to 
+          #   {Collectible#paginated_resource} and {Linkable#self_link}
+          # @return [Hash] The resource
+          def as_resource(options = {})
+            (dataset_pages, dateset_resource) = 
+              paginated_resource(objects_dataset, options)
+
+            {
+
+            }
+          end
+
+          # Returns a constant indicating the orientation of the image
+          #
+          # @return [Boolean] ORIENTATION_LANDSCAPE if the width it greater than
+          #   or equal to the height, ORIENTATION_PORTRAIT if the height is
+          #   greater than the width. NB: a square image will be called 
+          #   landscape
+          def orientation
+            if pixelw >= pixelh
+              return ORIENTATION_LANDSCAPE
+            end
+
+            return ORIENTATION_PORTRAIT
+          end
+
+          # Answers the question, "Is this image landscape?"
+          #
+          # @return [Boolean] True if the width of the image is greater than or
+          #   equal to the height. NB: This will return true for square images.
+          def is_landscape?
+            return orientation == ORIENTATION_LANDSCAPE
+          end
+
+          # Answers the question, "Is this image portrait?"
+          #
+          # @return [Boolean] True if the height of the image is greater than 
+          #   the width. NB: This will return false for square images.
+          def is_portrait?
+            return orientation == ORIENTATION_PORTRAIT
+          end
+        end 
       end
     end
   end
--- spec/media_spec.rb
+++ spec/media_spec.rb
@@ -6,6 +6,37 @@
 
 require 'spec_helper'
 
+describe MDL::Media do
+  it "returns rows" do
+    # There are 1044 media formats as of Nov 2012
+    MDL::Media.all().count.should be >= 1044
+  end
+
+  context "landscape oriented image" do
+    before :all do
+      @landscape = MDL::Media[47378]
+    end
+
+    it "is landscape" do
+      @landscape.orientation.should eq MDL::Media::ORIENTATION_LANDSCAPE
+      @landscape.is_landscape?.should eq true
+      @landscape.is_portrait?.should eq false
+    end
+  end
+
+  context "portrait oriented image" do
+    before :all do
+      @portrait = MDL::Media[47380]
+    end
+
+    it "is portrait" do
+      @portrait.orientation.should eq MDL::Media::ORIENTATION_PORTRAIT
+      @portrait.is_landscape?.should eq false
+      @portrait.is_portrait?.should eq true
+    end
+  end
+end
+
 describe MDL::MediaFormat do
   it "should return rows" do
     # There are 24 media formats as of Aug 2012
